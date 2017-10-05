ddb85674c5babbb0298457fb1a4841f0b8c2f851
Søren Roed Schack <soer622t@edu.easj.dk>
2017-10-05 14:25:23 +0200

    Add project files.

---
+++ uge40Peter_ApartmentRestConsumerConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uge40Peter_ApartmentRestConsumerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
